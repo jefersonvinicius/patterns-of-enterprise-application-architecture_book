7ea40b7cada9f0c685506720ae715f2d8748d340
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2023-06-19 20:18:31 -0300

    Finish table data gateway pattern

--- 02-data-source-architectural/04-table-data-gateway/gateway.ts
+++ 02-data-source-architectural/04-table-data-gateway/gateway.ts
@@ -25,4 +25,46 @@ export class PersonTableGateway {
     const rows = await database.instance().all(sql);
     return rows;
   }
+
+  async findRow(id: number): Promise<PersonRow | null> {
+    const sql = `SELECT * FROM people WHERE id = ?`;
+    const row = await database.instance().get(sql, id);
+    return row || null;
+  }
+
+  async update(id: number, data: Partial<PersonRow>): Promise<PersonRow> {
+    const current = await this.findRow(id);
+    if (!current) throw new Error(`Trying update nonexistent row ${id}`);
+    const updated: PersonRow = {
+      id: current.id,
+      first_name: data.first_name ?? current.first_name,
+      last_name: data.last_name ?? current.last_name,
+      number_of_dependents: data.number_of_dependents ?? current.number_of_dependents,
+    };
+    const sql = `
+      UPDATE people 
+      SET first_name = ?, last_name = ?, number_of_dependents = ?
+      WHERE id = ?
+    `;
+    await database.instance().run(sql, updated.first_name, updated.last_name, updated.number_of_dependents, updated.id);
+    return updated;
+  }
+
+  async insert(data: Omit<PersonRow, 'id'>) {
+    const sql = `
+      INSERT INTO people (first_name, last_name, number_of_dependents)
+      VALUES (?,?,?)
+    `;
+    const result = await database.instance().run(sql, data.first_name, data.last_name, data.number_of_dependents);
+    const inserted: PersonRow = {
+      id: result.lastID!,
+      ...data,
+    };
+    return inserted;
+  }
+
+  async delete(id: number) {
+    const sql = `DELETE FROM people WHERE id = ?`;
+    await database.instance().run(sql, id);
+  }
 }
--- 02-data-source-architectural/04-table-data-gateway/index.ts
+++ 02-data-source-architectural/04-table-data-gateway/index.ts
@@ -11,21 +11,80 @@ describe('PersonTableGateway', () => {
   });
 
   it('should find all people', async () => {
-    const peopĺe = await personTable.findAll();
-    assert.deepStrictEqual(peopĺe.length, 5);
+    const people = await personTable.findAll();
+    assert.deepStrictEqual(people.length, 5);
   });
 
   it('should find with last name', async () => {
-    const peopĺe = await personTable.findWithLastName('Santos');
-    assert.deepStrictEqual(peopĺe.length, 2);
-    const lastNames = peopĺe.map((p) => p.last_name);
+    const people = await personTable.findWithLastName('Santos');
+    assert.deepStrictEqual(people.length, 2);
+    const lastNames = people.map((p) => p.last_name);
     const expectedLastNames = ['Santos', 'Santos'];
     assert.deepStrictEqual(lastNames, expectedLastNames);
   });
 
-  it('should find with last name', async () => {
+  it('should find with where', async () => {
     const people = await personTable.findWhere('number_of_dependents > 2');
     assert.deepStrictEqual(people.length, 2);
     people.forEach((p) => assert.strictEqual(p.number_of_dependents > 2, true));
+  });
+
+  it('should find a row', async () => {
+    const person = await personTable.findRow(1);
+    assert.deepStrictEqual(person, {
+      id: 1,
+      first_name: 'Jeferson',
+      last_name: 'Santos',
+      number_of_dependents: 6,
+    });
+    const personNotFound = await personTable.findRow(9999);
+    assert.deepStrictEqual(personNotFound, null);
+  });
+
+  it('should update person', async () => {
+    const person = await personTable.findRow(1);
+    assert.deepStrictEqual(person, {
+      id: 1,
+      first_name: 'Jeferson',
+      last_name: 'Santos',
+      number_of_dependents: 6,
+    });
+    const newData = { first_name: 'Jefferson', last_name: 'Santtos', number_of_dependents: 2 };
+    const updated = await personTable.update(person.id, newData);
+    assert.deepStrictEqual(updated, {
+      id: 1,
+      ...newData,
+    });
+    assert.deepStrictEqual(await personTable.findRow(1), {
+      id: 1,
+      ...newData,
+    });
+  });
+
+  it('should throws if try update nonexistent ', async () => {
+    const promise = personTable.update(99999, { first_name: 'Any' });
+    assert.rejects(promise, { message: 'Trying update nonexistent row 99999' });
+  });
+
+  it('should insert row', async () => {
+    const person = await personTable.insert({
+      first_name: 'New',
+      last_name: 'Cool',
+      number_of_dependents: 1,
+    });
+    assert.deepStrictEqual(person, {
+      id: 6,
+      first_name: 'New',
+      last_name: 'Cool',
+      number_of_dependents: 1,
+    });
+  });
+
+  it('should delete', async () => {
+    const person = await personTable.findRow(1);
+    assert.deepStrictEqual(typeof person, 'object');
+    await personTable.delete(1);
+    const deleted = await personTable.findRow(1);
+    assert.deepStrictEqual(deleted, null);
   });
 });
