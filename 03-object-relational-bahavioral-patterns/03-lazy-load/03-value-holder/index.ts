2834ba8136ee6ef1828105f65c62f25753044ef5
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2023-08-02 21:41:37 -0300

    Added ValueHolder lazy loading technique

--- 03-object-relational-bahavioral-patterns/03-lazy-load/03-value-holder/index.ts
+++ 03-object-relational-bahavioral-patterns/03-lazy-load/03-value-holder/index.ts
@@ -17,4 +17,75 @@ class ValueHolder<T> {
   }
 }
 
-class Supplier {}
+class Product {
+  constructor(readonly id: number, readonly name: string, readonly price: number) {}
+}
+
+class Supplier {
+  private products!: ValueHolder<Product[]>;
+
+  constructor(readonly id: number, readonly name: string) {}
+
+  getProducts() {
+    return this.products.getValue();
+  }
+
+  setProducts(valueHolder: ValueHolder<Product[]>) {
+    this.products = valueHolder;
+  }
+}
+
+class ProductMapper {
+  private static instance: ProductMapper | null = null;
+
+  static create() {
+    if (this.instance === null) this.instance = new ProductMapper();
+    return this.instance;
+  }
+
+  async findForSupplier(supplierId: number) {
+    const sql = 'SELECT * FROM products WHERE supplier_id = ?';
+    const rows = await database.instance().all(sql, supplierId);
+    return rows.map((row) => new Product(row.id, row.name, row.price));
+  }
+}
+
+class SupplierMapper {
+  static ProductLoader = class implements ValueLoader<Product[]> {
+    constructor(readonly supplierId: number) {}
+
+    load(): Promise<Product[]> {
+      return ProductMapper.create().findForSupplier(this.supplierId);
+    }
+  };
+
+  async findById(id: number) {
+    const sql = 'SELECT * FROM suppliers WHERE id = ?';
+    const row = await database.instance().get(sql, id);
+    const supplier = new Supplier(row.id, row.name);
+    supplier.setProducts(new ValueHolder(new SupplierMapper.ProductLoader(supplier.id)));
+    return supplier;
+  }
+}
+
+describe('Supplier', () => {
+  before(async () => {
+    await database.start();
+  });
+
+  it('should load products once', async () => {
+    const findForSupplierSpy = mock.method(ProductMapper.create(), 'findForSupplier');
+    const supplierMapper = new SupplierMapper();
+    const supplier = await supplierMapper.findById(1);
+    assert.strictEqual(supplier.id, 1);
+    const products = await supplier.getProducts();
+    assert.strictEqual(products.length, 4);
+    assert.deepStrictEqual(
+      products.map((p) => p.name),
+      ['Xbox One', 'Book', 'Laptop', 'MacBook']
+    );
+    const productsAgain = await supplier.getProducts();
+    assert.deepStrictEqual(products, productsAgain);
+    assert.strictEqual(findForSupplierSpy.mock.callCount(), 1);
+  });
+});
--- 03-object-relational-bahavioral-patterns/03-lazy-load/03-value-holder/schema.sql
+++ 03-object-relational-bahavioral-patterns/03-lazy-load/03-value-holder/schema.sql
@@ -1,8 +1,23 @@
-CREATE TABLE people (
+CREATE TABLE products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255),
+  price DECIMAL,
+  supplier_id INTEGER,
+  FOREIGN KEY(supplier_id) REFERENCES suppliers(id)
+);
+
+CREATE TABLE suppliers (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
 );
 
-INSERT INTO people (name) VALUES 
+INSERT INTO suppliers (name) VALUES 
   ("Jeferson"),
   ("Kratos");
+
+INSERT INTO products (name, price, supplier_id) VALUES
+  ("Xbox One", 2000, 1),
+  ("Book", 50, 1),
+  ("Laptop", 5000, 1),
+  ("MacBook", 10000, 1),
+  ("Cachaça", 10, 2);
