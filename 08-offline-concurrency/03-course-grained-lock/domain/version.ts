b3dfba70310a0bc6b1ce65c80ecf940efb12124f
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2024-11-20 18:28:08 -0300

    Implements insert method of Course Grained Lock

--- 08-offline-concurrency/03-course-grained-lock/domain/version.ts
+++ 08-offline-concurrency/03-course-grained-lock/domain/version.ts
@@ -2,19 +2,33 @@ import database from '../infra/database';
 import { AppSessionManager } from './session';
 
 export class Version {
+  static NO_ID = -1;
+
   constructor(
-    readonly id: number,
-    readonly value: number,
+    private _id: number,
+    public value: number,
     readonly modifiedBy: string,
     readonly modifiedAt: string,
-    readonly locked: boolean,
-    readonly isNew: boolean
+    public locked: boolean
   ) {}
 
   static UPDATE_SQL = 'UPDATE versions SET value = ?, modifiedBy = ?, modifiedAt = ? WHERE id = ? AND value = ?';
   static DELETE_SQL = 'DELETE versions WHERE id = ? AND value = ?';
-  static INSERT_SQL = 'INSERT INTO versions (value, modifiedBy, modifiedAt) VALUES (?, ?, ?, ?)';
+  static INSERT_SQL = 'INSERT INTO versions (value, modifiedBy, modifiedAt) VALUES (?, ?, ?)';
   static LOAD_SQL = 'SELECT * FROM versions WHERE id = ?';
+
+  get id() {
+    return this._id;
+  }
+
+  get isNew() {
+    return this.id === Version.NO_ID;
+  }
+
+  static create(modifiedByUser: string) {
+    const version = new Version(this.NO_ID, 0, modifiedByUser, new Date().toISOString(), false);
+    return version;
+  }
 
   static async find(id: number) {
     let version = AppSessionManager.identityMap.getVersion(id);
@@ -24,11 +38,40 @@ export class Version {
     return version;
   }
 
+  async insert() {
+    if (!this.isNew) return;
+    const result = await database.instance().run(Version.INSERT_SQL, this.value, this.modifiedBy, this.modifiedAt);
+    if (result.lastID) {
+      this._id = result.lastID;
+      return true;
+    }
+    return false;
+  }
+
+  async increment() {
+    if (this.locked) return;
+
+    const params = [this.value + 1, this.modifiedBy, this.modifiedAt, this.id, this.value];
+    const result = await database.instance().run(Version.UPDATE_SQL, ...params);
+
+    if (result.changes === 0) {
+      await this.throwConcurrencyException();
+    }
+
+    this.value++;
+    this.locked = true;
+  }
+
   private static async load(id: number) {
     const row = await database.instance().get(this.LOAD_SQL, id);
     if (!row) return null;
-    const version = new Version(row.id, row.value, row.modifiedBy, row.modifiedAt, true, false);
+    const version = new Version(row.id, row.value, row.modifiedBy, row.modifiedAt, false);
     AppSessionManager.identityMap.putVersion(version);
     return version;
+  }
+
+  private async throwConcurrencyException() {
+    const currentVersion = await Version.load(this.id);
+    throw new Error(`Version modified by ${currentVersion?.modifiedBy} at ${currentVersion?.modifiedAt}`);
   }
 }
--- 08-offline-concurrency/03-course-grained-lock/index.ts
+++ 08-offline-concurrency/03-course-grained-lock/index.ts
@@ -1,15 +1,32 @@
-import { before, describe, it } from 'node:test';
+import { before, describe, it, mock } from 'node:test';
 import { Version } from './domain/version';
 import assert from 'node:assert';
 import database from './infra/database';
 
 describe('Version', () => {
+  const date = new Date();
+
   before(async () => {
     await database.start();
   });
 
+  it('should insert version', async () => {
+    mock.timers.enable({ apis: ['Date'], now: date });
+    const version = Version.create('jeferson');
+    const result = await version.insert();
+
+    assert.strictEqual(result, true);
+    assert.deepStrictEqual(version, new Version(1, 0, 'jeferson', date.toISOString(), false));
+
+    mock.timers.reset();
+  });
+
   it('should load version', async () => {
     const version = await Version.find(1);
-    assert.deepStrictEqual(version, new Version(1, 999, 'jeferson', new Date().toISOString(), true, true));
+    assert.deepStrictEqual(version, new Version(1, 0, 'jeferson', date.toISOString(), false));
+  });
+
+  it('should increment version', async () => {
+    // TODO
   });
 });
